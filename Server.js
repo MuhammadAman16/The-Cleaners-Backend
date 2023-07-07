4bc0e047139cceadf2eca497874d7935375b4e54
MuhammadAman16 <mohammedaman5253@gmail.com>
2023-07-07 12:20:50 +0500

    modified

--- Server.js
+++ Server.js
@@ -17,7 +17,7 @@ connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
   console.log("The solution is: ", results[0].solution);
 });
 
-app.get("/users", function (req, res) {
+app.get("/api/users", function (req, res) {
   connection.query("SELECT * FROM users", function (error, rows, fields) {
     if (error) {
       console.log(error);
@@ -25,6 +25,54 @@ app.get("/users", function (req, res) {
     } else {
       console.log(rows);
       res.json(rows);
+    }
+  });
+});
+app.get("/api/users/:id", function (req, res) {
+  const userId = req.params.id;
+  connection.query(
+    "SELECT * FROM users WHERE id = ?",
+    [userId],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      } else {
+        console.log(rows);
+        res.json(rows);
+      }
+    }
+  );
+});
+
+app.get("/api/orders", (req, res) => {
+  const { images, name, address, phone, shirts, pants, instructions, userid } =
+    req.body;
+
+  const insertQuery = `INSERT INTO orders ( images, name, address, phone, shirts, pants, instructions, userid) 
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  const values = [
+    images,
+    name,
+    address,
+    phone,
+    shirts,
+    pants,
+    instructions,
+    userid,
+  ];
+
+  connection.query(insertQuery, values, (error, results) => {
+    if (error) {
+      console.error("Error inserting data into orders:", error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while inserting data into orders." });
+    } else {
+      console.log("Data inserted into orders:", results);
+      res
+        .status(200)
+        .json({ message: "Data successfully inserted into orders." });
     }
   });
 });
